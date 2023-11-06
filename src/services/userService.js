5ea9c2c6225bd4d1a327df2828293f7e97563a12
DatsunCo <danya200375d@gmail.com>
2023-11-06 13:55:47 +0300

    ServerTest

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,11 @@ import $api from "../http";
 export default class AuthService{
 
     static async getSubscriptionsPlan(){
-        return $api.get(`/plans/usd-crypto/initial/available`)
+        //return $api.get(`/plans/usd-crypto/initial/available`)
+        return await axios
+            .post('https://crypto.cmd-root.com/api/plans/usd-crypto/initial/available',
+            { withCredentials: true }
+        )
         
     }
 
