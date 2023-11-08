fb96024cf7c410bfc19fa28058f2f34851f6059f
DatsunCo <danya200375d@gmail.com>
2023-11-08 12:52:55 +0300

    add  getChekoutDetails & getPaymantTokens

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,34 +1,40 @@
 import $api from "../http";
 import axios from "axios";
 
-export default class AuthService{
+export default class AuthService {
 
-    static async getSubscriptionsPlan(){
+    static async getSubscriptionsPlan() {
         //return $api.get(`/plans/usd-crypto/initial/available`)
         return axios.get('https://crypto.cmd-root.com/api/plans/usd-crypto/initial/available')
-        
+
     }
 
-    static async getPaymentTokens(){
+    static async getPaymentTokens() {
         return $api.get(`/billing/internal/crypto/payment-tokens`)
-        
+
     }
 
-    static async getReferalCode(referal){
+    static async getReferalCode(referal) {
         //return $api.get(`https://crypto.cmd-root.com/api/user/referral?referralCode=${referal}`)
         return axios.get(`https://crypto.cmd-root.com/api/user/referral?referralCode=${referal}`)
-        
+
     }
 
-    static async postReservation(email, plan_id, referral_code){
+    static async postReservation(email, plan_id, referral_code) {
 
-        return $api.post(`/checkout/generate`, { email, plan_id})
-        
+        return $api.post(`/checkout/generate`, { email, plan_id })
+                .then(response => {
+                    const cookies = response.headers['set-cookie'];
+                    console.log(cookies)
+                    // сохраняем значение cookie в localStorage
+                    localStorage.setItem('session', cookies);
+                })
+
     }
 
-    static async getChekoutDetails(){
+    static async getChekoutDetails() {
 
         return $api.get(`/checkout/details`)
-        
+
     }
 }
