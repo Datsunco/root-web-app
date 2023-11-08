33237f1e232a10e855200b3947efe435270f48ac
DatsunCo <danya200375d@gmail.com>
2023-11-08 14:11:28 +0300

    add  getChekoutDetails & getPaymantTokens

--- src/services/userService.js
+++ src/services/userService.js
@@ -23,13 +23,14 @@ export default class AuthService {
     static async postReservation(email, plan_id, referral_code) {
 
         return $api.post(`/checkout/generate`, { email, plan_id })
-                // .then(response => {
-                //     const cookies = response.headers['Set-Cookie:'];
-                //     console.log(cookies, "cookies")
-                //     console.log(response.cookies, "cookies")
-                //     // сохраняем значение cookie в localStorage
-                //     localStorage.setItem('session', cookies);
-                // })
+                .then(response => {
+                    const cookies = response.headers['Set-Cookie:'];
+                    console.log(response.headers, "headers")
+                    console.log(cookies, "cookies")
+                    console.log(response.cookies, "cookies")
+                    // сохраняем значение cookie в localStorage
+                    localStorage.setItem('session', cookies);
+                })
 
     }
 
