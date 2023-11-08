10fcfadb0fe110bd2bc0b8e3b638e0dc69f8e7f0
DatsunCo <danya200375d@gmail.com>
2023-11-08 14:00:19 +0300

    add  getChekoutDetails & getPaymantTokens

--- src/http/index.js
+++ src/http/index.js
@@ -7,9 +7,9 @@ const $api = axios.create({
 })
 
 
-$api.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
-    return config;
-})
+// $api.interceptors.request.use((config) => {
+//     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+//     return config;
+// })
 
 export default $api
